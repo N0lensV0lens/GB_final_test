4f13ff59a20bb17e35b16019f1fbd4f579d53fc7
Rakhmatilla <coldskin123@yandex.com>
2024-11-16 22:31:31 +0300

    Added a function for input size

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static int GetUserInputSize()
+    {
+        Console.WriteLine("___Программа для вывода строк, длина которых меньше, либо равна 3 символам___");
+        while (true)
+        {
+            // Запрос длины массива
+            Console.WriteLine("Укажи размер массива: ");
+            string size = Console.ReadLine();
+
+            if (int.TryParse(size, out int number) && number > 0)
+            {
+                return number;
+            }
+
+            Console.WriteLine(number <= 0 ? "Введи положительное число" : "Нужно ввести числовое значение.");
+        }
+    }
+
